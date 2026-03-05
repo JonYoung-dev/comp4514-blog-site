9ae06af1e70ed63d308456ae02a0885b6c806fa3
J-Young <jyoung222@my.bcit.ca>
2026-03-05 13:54:00 -0800

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace comp4514_blog_site.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
